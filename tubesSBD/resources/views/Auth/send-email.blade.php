b430f46a2637bc2a9fc63441a00d023a078b3c41
Steven Valentino <huangwieyang@gmail.com>
2022-05-14 23:04:07 +0700

    test, error before

--- tubesSBD/resources/views/Auth/send-email.blade.php
+++ tubesSBD/resources/views/Auth/send-email.blade.php
@@ -3,7 +3,7 @@
 <div class="container">
     <h2>Token have been sent to {{ session('email') }}</h2>
     <form action="/resend/{{ session('email') }}">
-        <img src="images/undraw_message_sent_re_q2kl.svg" alt="" srcset="">
+        <img src="images/undraw_message_sent_re_q2kl.jpg" alt="" srcset="">
         <h3>We've sent an email to you, check your email box</h3>
         <h4>If you didn't recieve the code, click the Resend Button</h4>
         <br>
