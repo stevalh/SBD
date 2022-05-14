d12978472b79578a3dcf7c570c8ccdd2ff949378
Steven Valentino <huangwieyang@gmail.com>
2022-05-14 21:44:37 +0700

    .

--- tubesSBD/resources/views/Auth/send-email.blade.php
+++ tubesSBD/resources/views/Auth/send-email.blade.php
@@ -1,10 +1,13 @@
 @include('AuthLayout.header')
 
-<h1>Token have been sent to {{ session('email') }}</h1>
-
-<form action="/resend/{{ session('email') }}" method="POST">
-    @csrf
-<h5>if you didn't recieve the code,Click Resend Button</h5>    
-<button type="submit">Resend</button>
-</form>
+<div class="container">
+    <h2>Token have been sent to {{ session('email') }}</h2>
+    <form action="/resend/{{ session('email') }}">
+        <img src="images/undraw_message_sent_re_q2kl.svg" alt="" srcset="">
+        <h3>We've sent an email to you, check your email box</h3>
+        <h4>If you didn't recieve the code, click the Resend Button</h4>
+        <br>
+        <button type="submit">Resend</button>
+    </form>
+</div>
 @include('AuthLayout.footer')
--- tubesSBD/resources/views/AuthLayout/header.blade.php
+++ tubesSBD/resources/views/AuthLayout/header.blade.php
@@ -1,17 +1,35 @@
-
 <!DOCTYPE html>
-<html lang="en" >
+<html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <title>Otp Veritication HTML template</title>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
-<link rel="stylesheet" href="./style.css">
- 
+    <meta charset="UTF-8">
+    <title>Verification Link | TraceTogether</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+    <link rel="stylesheet" href="./style.css">
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+
+        }
+
+        body {
+            background-color: white;
+        }
+
+        .container {
+            position: absolute;
+            border: 1px solid black;
+            left: 50%;
+            top: 50%;
+            transform: translate(-50%, -50%);
+            padding: 10px;
+        }
+        .container img{
+            width: 50%;
+            height: 50%;
+
+        }
+    </style>
 </head>
 <body>
-
-       
- 
-        
-        
- 
