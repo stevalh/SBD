583e366fcf866baaa4906aea3c3486a2f38f1593
Steven Valentino <huangwieyang@gmail.com>
2022-05-22 11:03:57 +0700

    .

--- tubesSBD/resources/views/editProfile.blade.php
+++ tubesSBD/resources/views/editProfile.blade.php
@@ -4,85 +4,83 @@
 <div class="container">
     @if (session()->has('message'))
     <div class="alert alert-danger " role="alert">
-    {{ session('message') }}
-    @endif
-    @if (session()->has('success'))
-    <div class="alert alert-success " role="alert">
-    {{ session('success') }}
-    @endif
-    </div>
+        {{ session('message') }}
+        @endif
+        @if (session()->has('success'))
+        <div class="alert alert-success " role="alert">
+            {{ session('success') }}
+            @endif
+        </div>
 
-    {{-- Profile --}}
-    <div class="main-banner wow fadeIn" id="top" data-wow-duration="1s" data-wow-delay="0.5s">
-        <div class="container">
-           
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="row">
-                        <div class="col-lg-6 align-self-center">
-                            <div class="left-content show-up header-text wow fadeInLeft" data-wow-duration="1s"
-                                data-wow-delay="1s">
-                                <div class="row">
-                                    <div class="col-lg-12">
-                                        <form class="" action="{{ route('profile.update') }}" method="POST">
-                                            @csrf
-                                            <span class="">
-                                                Update Profile
-                                            </span>
-                                            <label for="fname"></label>
-                                            <div class="wrap-input100 validate-input" >
-                                                <input class="fname input100  " type="text"
-                                                    value="{{ auth()->user()->fname }}" name="fname" placeholder="Full Name" required>
-                                                @error('fname')
+        {{-- Profile --}}
+        <div class="main-banner wow fadeIn" id="top" data-wow-duration="1s" data-wow-delay="0.5s">
+            <div class="container">
+
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="row">
+                            <div class="col-lg-6 align-self-center">
+                                <div class="left-content show-up header-text wow fadeInLeft" data-wow-duration="1s"
+                                    data-wow-delay="1s">
+                                    <div class="row">
+                                        <div class="col-lg-12">
+                                            <form class="" action="{{ route('profile.update') }}" method="POST">
+                                                @csrf
+                                                <span class="">
+                                                    Update Profile
+                                                </span>
+                                                <label for="fname"></label>
+                                                <div class="wrap-input100 validate-input">
+                                                    <input class="fname input100  " type="text"
+                                                        value="{{ auth()->user()->fname }}" name="fname"
+                                                        placeholder="Full Name" required>
+                                                    @error('fname')
                                                     <div class="fname-validation" style="color: red">
                                                         {{ $message }}
                                                     </div>
-                                                @enderror
-                                                <span class="focus-input100"></span>
-                                                <span class="symbol-input100">
-                                                    <i class="fa fa-user" aria-hidden="true"></i>
-                                                </span>
-                                            </div>
-                                            <label for="NIK"></label>
-                                            <div class="wrap-input100 validate-input" >
-                                                <input class="NIK input100 " type="text"
-                                                    value="{{  auth()->user()->NIK }}" name="NIK" placeholder="NIK" required>
-                                                @error('NIK')
+                                                    @enderror
+                                                    <span class="focus-input100"></span>
+                                                    <span class="symbol-input100">
+                                                        <i class="fa fa-user" aria-hidden="true"></i>
+                                                    </span>
+                                                </div>
+                                                <label for="NIK"></label>
+                                                <div class="wrap-input100 validate-input">
+                                                    <input class="NIK input100 " type="text"
+                                                        value="{{  auth()->user()->NIK }}" name="NIK" placeholder="NIK"
+                                                        required>
+                                                    @error('NIK')
                                                     <div class="NIK-validation " style="color: red">
                                                         {{ $message }}
                                                     </div>
-                                                @enderror
-                                                <span class="focus-input100"></span>
-                                                <span class="symbol-input100">
-                                                    <i class="fa fa-user" aria-hidden="true"></i>
-                                                </span>
-                                            </div>
-                                            <label for="email"></label>
-                        
-                                            
+                                                    @enderror
+                                                    <span class="focus-input100"></span>
+                                                    <span class="symbol-input100">
+                                                        <i class="fa fa-user" aria-hidden="true"></i>
+                                                    </span>
+                                                </div>
+                                                <label for="email"></label>
                                                 <button type="submit" class="btn btn-primary">
-                                                 Update
+                                                    Update
                                                 </button>
-                                            
-                        
-                                            
-                                        </form>
-                                        {{-- End Profile Menu --}}
+                                                <a href="/profile" style="color: #ababab">Back to profile</a>
+                                            </form>
+                                            {{-- End Profile Menu --}}
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="col-lg-6">
-                            <div class="right-image wow fadeInRight" data-wow-duration="1s" data-wow-delay="0.5s">
-                                <img src="assets/images/slider-dec.png" alt="">
+                            <div class="col-lg-6">
+                                <div class="right-image wow fadeInRight" data-wow-duration="1s" data-wow-delay="0.5s">
+                                    <img src="assets/images/slider-dec.png" alt="">
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    {{-- End Profile --}}
+        {{-- End Profile --}}
 
-    <script></script>
-@endsection
+        <script></script>
+        @endsection
--- tubesSBD/resources/views/profile.blade.php
+++ tubesSBD/resources/views/profile.blade.php
@@ -1,54 +1,51 @@
 @extends('Main.format')
 
 @section('content')
-    @include('Layout.navbar')
-    {{-- Profile --}}
-    <div class="main-banner wow fadeIn" id="top" data-wow-duration="1s" data-wow-delay="0.5s">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="row">
-                        <div class="col-lg-6 align-self-center">
-                            <div class="left-content show-up header-text wow fadeInLeft" data-wow-duration="1s"
-                                data-wow-delay="1s">
-                                <div class="row">
-                                    <div class="col-lg-12">
-                                        @auth
-                                            <h6>Hi, {{ auth()->user()->fname }}</h6>
-                                        @else
-                                            <h6>Hi</h6>
-                                        @endauth
-                                        {{-- Profile Menu --}}
-                                       
-                                            <div class="row">
-                                                <div class="col-6">
-                                                    <div class="profile-menu">
-                                                        <ul class="nav">
-                                                            <li><a href="{{ route('profile.edit') }}">Edit Profile</a></li>
-                                                            <li><a href="#">COVID-19 Test</a></li>
-                                                            <li><a href="#">Vaccination History</a></li>
-                                                            <li><a href="#">Vaccine Certificate</a></li>
-                                                        </ul>
-                                                    </div>
-                                                </div>
+@include('Layout.navbar')
+{{-- Profile --}}
+<div class="main-banner wow fadeIn" id="top" data-wow-duration="1s" data-wow-delay="0.5s">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="row">
+                    <div class="col-lg-6 align-self-center">
+                        <div class="left-content show-up header-text wow fadeInLeft" data-wow-duration="1s"
+                            data-wow-delay="1s">
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    @auth
+                                    <h6>Hi, {{ auth()->user()->fname }}</h6>
+                                    @else
+                                    <h6>Hi</h6>
+                                    @endauth
+                                    {{-- Profile Menu --}}
+                                    <div class="row">
+                                        <div class="col-6">
+                                            <div class="profile-menu">
+                                                <ul class="nav">
+                                                    <li><a href="{{ route('profile.edit') }}">Edit Profile</a></li>
+                                                    <li><a href="#">COVID-19 Test</a></li>
+                                                    <li><a href="#">Vaccine Certificate</a></li>
+                                                </ul>
                                             </div>
                                         </div>
-                                        {{-- End Profile Menu --}}
                                     </div>
                                 </div>
+                                {{-- End Profile Menu --}}
                             </div>
                         </div>
-                        <div class="col-lg-6">
-                            <div class="right-image wow fadeInRight" data-wow-duration="1s" data-wow-delay="0.5s">
-                                <img src="assets/images/slider-dec.png" alt="">
-                            </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="right-image wow fadeInRight" data-wow-duration="1s" data-wow-delay="0.5s">
+                            <img src="assets/images/slider-dec.png" alt="">
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    {{-- End Profile --}}
+</div>
+{{-- End Profile --}}
 
-    <script></script>
+<script></script>
 @endsection
